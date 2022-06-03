a31cd353b1c82148f65665f25dcaa1ba615a4412
Patrick Abele <janpatrix@gmail.com>
2022-06-03 11:33:31 +0200

    Added Project 13

--- Project 11/Project 11/GameScene.swift
+++ Project 11/Project 11/GameScene.swift
@@ -8,20 +8,67 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
+    private var ballList = ["ballBlue", "ballCyan", "ballGreen", "ballGrey", "ballPurple", "ballYellow", "ballRed"]
+        
+    var slotBase: SKSpriteNode!
+    var slotGlow: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
+    var ballLabel: SKLabelNode!
+    var editLabel: SKLabelNode!
+    var editingMode: Bool = false {
+        didSet {
+            if editingMode {
+                editLabel.text = "Done"
+            } else {
+                editLabel.text = "Edit"
+            }
+        }
+    }
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    var ballCount = 5 {
+        didSet {
+            ballLabel.text = "Balls Left: \(ballCount)"
+        }
+    }
     
     override func didMove(to view: SKView) {
         
+        physicsWorld.contactDelegate = self
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+
         let background = SKSpriteNode(imageNamed: "background@2x.jpg")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
+
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.position = CGPoint(x: 980, y: 700)
+        addChild(scoreLabel)
         
-        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        ballLabel = SKLabelNode(fontNamed: "Chalkduster")
+        ballLabel.text = "Balls Left: 5"
+        ballLabel.horizontalAlignmentMode = .right
+        ballLabel.position = CGPoint(x: 580, y: 700)
+        addChild(ballLabel)
+        
+    
+        editLabel = SKLabelNode(fontNamed: "Chalkduster")
+        editLabel.text = "Edit"
+        editLabel.position = CGPoint(x: 80, y: 700)
+        addChild(editLabel)
         
         makeBouncer(at: CGPoint(x: 0, y: 0))
         makeBouncer(at: CGPoint(x: 256, y: 0))
@@ -33,19 +80,91 @@ class GameScene: SKScene {
         makeSlot(at: CGPoint(x: 384, y: 0), isGood: false)
         makeSlot(at: CGPoint(x: 640, y: 0), isGood: true)
         makeSlot(at: CGPoint(x: 896, y: 0), isGood: false)
+                
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
         
+        guard let nodeA = contact.bodyA.node else {return}
+        guard let nodeB = contact.bodyB.node else {return}
+        
+        if nodeA.name == "ball" {
+            collisionBetween(ball: nodeA , object: nodeB)
+        } else if nodeB.name == "ball" {
+            collisionBetween(ball: nodeB, object: nodeA)
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         if let touch = touches.first {
             let location = touch.location(in: self)
-            let ball = SKSpriteNode(imageNamed: "ballRed")
-            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
-            ball.physicsBody?.restitution = 0.4
-            ball.position = location
-            addChild(ball)
+            let objects = nodes(at: location)
+            
+            if objects.contains(editLabel) {
+                editingMode.toggle()
+            } else {
+                if editingMode {
+                    createBox(at: location)
+                } else {
+                    makeBall(at: location)
+                }
+            }
         }
+    }
+    
+    func createBox(at position: CGPoint) {
+        let size = CGSize(width: Int.random(in: 16...128), height: 16)
+        let box = SKSpriteNode(color: UIColor(red: CGFloat.random(in:
+        0...1), green: CGFloat.random(in: 0...1), blue:
+        CGFloat.random(in: 0...1), alpha: 1), size: size)
+        box.position = position
+        box.zRotation = CGFloat.random(in: 0...3)
+        box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
+        box.physicsBody?.isDynamic = false
+        box.name = "box"
+        addChild(box)
+    }
+    
+    func makeBall(at position: CGPoint) {
+        
+        if ballCount == 0 {
+            ballCount = 5
+            enumerateChildNodes(withName: "box", using: { (node, stop) in
+                node.removeFromParent()
+            })
+        } else {
+            ballCount -= 1
+        }
+        
+        let ball = SKSpriteNode(imageNamed: ballList.randomElement()!)
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+        ball.physicsBody?.restitution = 0.4
+        ball.physicsBody!.contactTestBitMask = ball.physicsBody!.collisionBitMask
+        ball.position.x = position.x
+        ball.position.y = frame.maxY - 100
+        
+        ball.name = "ball"
+        addChild(ball)
+    }
+    
+    func collisionBetween(ball: SKNode, object: SKNode) {
+        if object.name == "good" {
+            destroy(ball: ball)
+            score += 1
+        } else if object.name == "bad" {
+            destroy(ball: ball)
+            score -= 1
+        }
+    }
+    
+    func destroy (ball: SKNode) {
+        if let fireParticles = SKEmitterNode(fileNamed: "FireParticles") {
+            fireParticles.position = ball.position
+            addChild(fireParticles)
+        }
+        
+        ball.removeFromParent()
     }
     
     func makeBouncer(at position: CGPoint) {
@@ -55,20 +174,23 @@ class GameScene: SKScene {
         bouncer.physicsBody?.isDynamic = false
         addChild(bouncer)
     }
-    
+        
     func makeSlot(at position: CGPoint, isGood: Bool) {
-        var slotBase: SKSpriteNode
-        var slotGlow: SKSpriteNode
         
         if isGood{
             slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+            slotBase.name = "good"
         } else {
             slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
             slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+            slotBase.name = "bad"
         }
         
         slotBase.position = position
+        slotBase.physicsBody = SKPhysicsBody(rectangleOf: slotBase.size)
+        slotBase.physicsBody?.isDynamic = false
+        
         slotGlow.position = position
         addChild(slotBase)
         addChild(slotGlow)
---
+++ Project 13/Project 13/Project_13App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_13App.swift
+//  Project 13
+//
+//  Created by Patrick Abele on 03.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Project_13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
