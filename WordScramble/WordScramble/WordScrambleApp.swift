3e7b5f9b2e41d611e94f43eca45c1273d39b6e89
Patrick Abele <janpatrix@gmail.com>
2022-11-29 09:06:47 +0100

    Word Scramble finalised

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Patrick Abele on 25.11.22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
